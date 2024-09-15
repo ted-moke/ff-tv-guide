d2d37f1bf77a750089f4548976b525d06695d2da
Ted Moke <iam@tedmoke.com>
2024-09-15 14:21:55 -0400

    Make splash page more decriptive of features

--- frontend/src/pages/SplashPage.tsx
+++ frontend/src/pages/SplashPage.tsx
@@ -3,27 +3,42 @@ import styles from "./SplashPage.module.css";
 import Button from "../components/ui/Button";
 import LinkButton from "../components/ui/LinkButton";
 import FFTVGLogo from "../assets/FFTVGLogo";
-import tvGuidePreview from "../assets/tv-guide-preview.png";
+import ssSingleMatch from "../assets/ss-single-match.png";
 
 const SplashPage: React.FC = () => {
   return (
     <div className={styles.splashContainer}>
-      <div className={styles.screenshotBackground}>
-        <img src={tvGuidePreview} alt="App Screenshot" />
-      </div>
-      <div className={styles.contentOverlay}>
-        <div className={styles.contentWrapper}>
-          <FFTVGLogo size="large" withText />
-          <div className={styles.descriptionContainer}>
-            <p>
-              Having trouble keeping track of which players you have in the
-              games on TV?
-            </p>
-            <h4>We've got you covered.</h4>
+      <div className={styles.contentWrapper}>
+        <FFTVGLogo size="large" withText />
+        <div className={styles.descriptionContainer}>
+          <p>
+            Having trouble keeping track of which players you have in the games
+            on TV?
+          </p>
+          <h4>We've got you covered.</h4>
+        </div>
+        <div className={styles.buttonContainer}>
+          <LinkButton to="/auth">Sign In</LinkButton>
+          <Button link="/auth?register=true">Create Account</Button>
+        </div>
+        <div className={styles.imageContainer}>
+          <div className={styles.keyFeatures}>
+            <h3>Key Features:</h3>
+            <ul>
+              <li>
+                Connect your fantas leagues from: Sleeper (more intergrations to
+                come)
+              </li>
+              <li>See which NFL games you have the most players in</li>
+              <li>Double check you bench players</li>
+            </ul>
           </div>
-          <div className={styles.buttonContainer}>
-            <LinkButton to="/auth">Sign In</LinkButton>
-            <Button link="/auth?register=true">Create Account</Button>
+          <div className={styles.previewContainer}>
+            <img
+              src={ssSingleMatch}
+              alt="TV Guide Preview"
+              className={styles.previewImage}
+            />
           </div>
         </div>
       </div>
