3c08fb3daeec08f3d1e334e1628c8e380e9088e8
Ted Moke <iam@tedmoke.com>
2025-09-05 22:51:39 -0400

    Touch up scroll ui

--- frontend/src/features/league/LeagueTicker.tsx
+++ frontend/src/features/league/LeagueTicker.tsx
@@ -8,32 +8,38 @@ export const LeagueTicker = () => {
   if (!userTeams || Object.keys(userTeams).length === 0) return null;
 
   return (
-    <div
-      className={`${styles.leagueTickerWrapper} ${styles.scrollbar} ${styles.scrollbarInvisible}`}
-    >
-      {Object.values(userTeams[CURRENT_SEASON]).map((team) => {
-        console.log("team", team);
-        const winning =
-          team.weekPoints != null &&
-          team.weekPointsAgainst != null &&
-          team.weekPoints > team.weekPointsAgainst;
+    <div className={`${styles.leagueTickerWrapper} ${styles.scrollContainer} ${styles.withGradient} ${styles.scrollbar} ${styles.scrollbarInvisible}`}>
+      <div
+        className={`${styles.leagueTickerContainer} ${styles.scrollbarInvisible}`}
+      >
+        {Object.values(userTeams[CURRENT_SEASON]).map((team) => {
+          const winning =
+            team.weekPoints != null &&
+            team.weekPointsAgainst != null &&
+            team.weekPoints > team.weekPointsAgainst;
           const losing =
-          team.weekPoints != null &&
-          team.weekPointsAgainst != null &&
-          team.weekPoints < team.weekPointsAgainst;
-        return (
-          <div key={team.id} className={`${styles.leagueTickerItem} ${winning ? styles.winning : losing ? styles.losing : styles.tied}`}>
-            <h3 className={styles.shortLeagueName}>{team.shortLeagueName}</h3>
+            team.weekPoints != null &&
+            team.weekPointsAgainst != null &&
+            team.weekPoints < team.weekPointsAgainst;
+          return (
             <div
-              className={styles.leagueTickerItemSubContent}
+              key={team.id}
+              className={`${styles.leagueTickerItem} ${
+                winning ? styles.winning : losing ? styles.losing : styles.tied
+              }`}
             >
-              <p className={styles.weekPoints}>{team.weekPoints?.toFixed(2)}</p>
-              <span>v</span>
-              <p className="muted">{team.weekPointsAgainst?.toFixed(2)}</p>
+              <h3 className={styles.shortLeagueName}>{team.shortLeagueName}</h3>
+              <div className={styles.leagueTickerItemSubContent}>
+                <p className={styles.weekPoints}>
+                  {team.weekPoints?.toFixed(2)}
+                </p>
+                <span>v</span>
+                <p className="muted">{team.weekPointsAgainst?.toFixed(2)}</p>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
